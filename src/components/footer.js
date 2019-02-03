5ff7cae53c94ce7271f3799b1ee08e9f4c230496
Matt Perry <mattgperry@gmail.com>
2019-02-03 13:07:55 +0000

    Updates

--- src/components/footer.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { ContentContainer } from "./bits";
-import styled from "styled-components";
-
-export default () => <ContentContainer as="footer" />;
--- src/components/themes.js
+++
@@ -1,8 +0,0 @@
-export const siteTheme = {
-  background: "#fff",
-  main: "#1fedcb",
-  secondary: "#31005C",
-  action: "#F5148C",
-  highlight: "rgba(33, 237, 203, 0.2)",
-  dark: "#333"
-};
